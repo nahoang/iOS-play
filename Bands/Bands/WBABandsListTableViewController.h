274be14da41b4cb14a8cead00730ea452236cf55
nahoang <nahoang9698@gmail.com>
2018-12-10 16:50:57 +0700

    feat(WBABandListTable): Displaying a UITableViewCell

---
+++ Bands/Bands/WBABandsListTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  WBABandsListTableViewController.h
+//  Bands
+//
+//  Created by hoang nguyen on 12/7/18.
+//  Copyright © 2018 hoang nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WBABandsListTableViewController : UITableViewController
+
+@end
